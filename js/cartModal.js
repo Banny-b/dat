b8325fcb2b2b861508dee2f2e89f2f40c25189cb
Sergey Petukhov <banny75@i.ua>
2023-08-25 17:43:51 +0300

    dat
    add animation modal window cart

--- js/cartModal.js
+++ js/cartModal.js
@@ -1,16 +1,34 @@
+// Code with smooth animation
 document.addEventListener("DOMContentLoaded", function () {
     const likeItemLink = document.querySelector(".cart-link");
-    const modal = document.getElementById("cart-mod");
+    const modal = document.querySelector(".cart-modal");
     const closeButton = document.querySelector(".basket__close-btn");
 
     likeItemLink.addEventListener("click", function (event) {
         event.preventDefault();
-        modal.style.display = "block";
+        modal.classList.add("active");
         closeButton.style.display = "block";
     });
 
     closeButton.addEventListener("click", function () {
-        modal.style.display = "none";
-        closeButton.style.display = "none";
+        modal.classList.remove("active");
     });
 });
+
+// Code without animation
+// document.addEventListener("DOMContentLoaded", function () {
+//     const likeItemLink = document.querySelector(".cart-link");
+//     const modal = document.getElementById("cart-mod");
+//     const closeButton = document.querySelector(".basket__close-btn");
+
+//     likeItemLink.addEventListener("click", function (event) {
+//         event.preventDefault();
+//         modal.style.display = "block";
+//         closeButton.style.display = "block";
+//     });
+
+//     closeButton.addEventListener("click", function () {
+//         modal.style.display = "none";
+//         closeButton.style.display = "none";
+//     });
+// });
